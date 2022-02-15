11f7bca097aa18f94d962c1fcdf5c733888a9826
Anton Kratok <kratok@mail.ru>
2022-02-15 16:20:07 +0300

    IRepository

---
+++ Contracts/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataContracts;
+
+namespace Contracts
+{
+    internal interface IRepository<T> where T:BaseEntity
+    {
+        internal IEnumerable<T> GetAll();
+        internal void Insert<T> ();
+    }
+}
